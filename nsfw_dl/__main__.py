4a04ef5ff1357ec9124f37a574e9343860a55741
AraHaan <seandhunt_7@yahoo.com>
2017-08-30 01:59:08 -0400

    Start on the commandline.

---
+++ nsfw_dl/__main__.py
@@ -0,0 +1,16 @@
+"""
+Read the license at:
+https://github.com/AraHaan/nsfw_dl/blob/master/LICENSE
+"""
+import sys
+
+
+def main(argv):
+    """
+    Main entrypoint to nsfw_dl commandline.
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
