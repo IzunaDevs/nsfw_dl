68480f191167573e32853427c37049fc6f7d1279
martmists <martmists@gmail.com>
2017-09-01 22:22:05 +0000

    Revert "Make it find this"
    This reverts commit f4d8589a9c424d32ae403eddd7d97366c27cb8fd.

--- nsfw_dl/__init__.py
+++ nsfw_dl/__init__.py
@@ -1,1 +1,13 @@
+"""
+Read the license at:
+https://github.com/IzunaDevs/nsfw_dl/blob/master/LICENSE
+"""
 
+from .dl import NSFWDL  # noqa
+
+__title__ = 'nsfw_dl'
+__author__ = 'IzunaDevs'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 IzunaDevs'
+__version__ = '0.2.0'
+__build__ = 0x000100
