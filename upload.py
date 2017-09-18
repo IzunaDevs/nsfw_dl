87e45394ed8600b65420d2efac16cd109a941584
martmists <martmists@gmail.com>
2017-09-19 00:07:55 +0200

    Fixes

--- upload.py
+++ upload.py
@@ -22,8 +22,8 @@ if ver != remote_ver:
     print("UPLOADING TO PYPI")
 
     p1 = subprocess.Popen(
-        "python3.6 setup.py build build_ext build_whl".split())
+        "python3.6 setup.py sdist bdist_egg bdist_wheel".split())
     p1.wait()
     p2 = subprocess.Popen(
-        f"twine upload build/*{ver}* -u {un} -p {pw}".split())
+        f"twine upload dist/*{ver}* -u {un} -p {pw}".split())
     p2.wait()
