b5f63730159095e91daf700f73683c11688b93e3
martmists <martmists@gmail.com>
2017-09-18 23:37:40 +0200

    Do a thing

--- upload.py
+++ upload.py
@@ -15,8 +15,12 @@ ver = nsfw_dl.__version__
 
 if ver != remote_ver:
     p1 = subprocess.Popen(
-            "python3.6 setup.py build buld_ext build_whl".split())
+            "python3.6 setup.py build buld_ext build_whl".split(),
+            stdout=subprocess.PIPE)
     p1.wait()
+    print(p1.stdout.read())
     p2 = subprocess.Popen(
-            f"twine upload build/*{ver}* -u {un} -p {pw}".split())
+            f"twine upload build/*{ver}* -u {un} -p {pw}".split(),
+            stdout=subprocess.PIPE)
     p2.wait()
+    print(p2.stdout.read())
