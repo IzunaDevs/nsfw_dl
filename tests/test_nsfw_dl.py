2a93284c91a468a73bf924e4aad3f982a8d5ebb2
martmists <martmists@gmail.com>
2017-09-19 00:00:23 +0200

    Fix Typo and fix tests

--- tests/test_nsfw_dl.py
+++ tests/test_nsfw_dl.py
@@ -14,7 +14,7 @@ def test_danbooru_random():
     """tests danbooru's random."""
     with nsfw_dl.NSFWDL() as dl:
         img = dl.download("DanbooruRandom")
-    assert img is not ""
+    assert img
 
 
 @pytest.mark.asyncio
@@ -22,142 +22,142 @@ async def test_danbooru_async():
     """ ONLY ASYNC TEST """
     async with nsfw_dl.NSFWDL() as dl:
         img = await dl.download("DanbooruRandom")
-    assert img is not ""
+    assert img
 
 
 def test_danbooru_search():
     with nsfw_dl.NSFWDL() as dl:
         img = dl.download("DanbooruSearch", args="1girl")
-        assert img is not ""
+        assert img
 
 
 def test_drunkenpumken_random():
     with nsfw_dl.NSFWDL() as dl:
         img = dl.download("DrunkenpumkenRandom")
-        assert img is not ""
+        assert img
 
 
 def test_e621_random():
     with nsfw_dl.NSFWDL() as dl:
         img = dl.download("E621Random")
-        assert img is not ""
+        assert img
 
 
 def test_e621_search():
     with nsfw_dl.NSFWDL() as dl:
         img = dl.download("E621Search", args="1girl")
-        assert img is not ""
+        assert img
 
 
 def test_furrybooru_random():
     with nsfw_dl.NSFWDL() as dl:
         img = dl.download("FurrybooruRandom")
-        assert img is not ""
+        assert img
 
 
 def test_furrybooru_search():
     with nsfw_dl.NSFWDL() as dl:
         img = dl.download("FurrybooruSearch", args="1girl")
-        assert img is not ""
+        assert img
 
 
 def test_gelbooru_random():
     with nsfw_dl.NSFWDL() as dl:
         img = dl.download("GelbooruRandom")
-        assert img is not ""
+        assert img
 
 
 def test_gelbooru_search():
     with nsfw_dl.NSFWDL() as dl:
         img = dl.download("GelbooruSearch", args="1girl")
-        assert img is not ""
+        assert img
 
 
 def test_hbrowse_random():
     with nsfw_dl.NSFWDL() as dl:
         img = dl.download("HbrowseRandom")
-        assert img is not ""
+        assert img
 
 
 def test_konachan_random():
     with nsfw_dl.NSFWDL() as dl:
         img = dl.download("KonachanRandom")
-        assert img is not ""
+        assert img
 
 
 def test_konachan_search():
     with nsfw_dl.NSFWDL() as dl:
         img = dl.download("KonachanSearch", args="2girls")
-        assert img is not ""
+        assert img
 
 
 def test_lolibooru_random():
     with nsfw_dl.NSFWDL() as dl:
         img = dl.download("LolibooruRandom")
-        assert img is not ""
+        assert img
 
 
 def test_lolibooru_search():
     with nsfw_dl.NSFWDL() as dl:
         img = dl.download("LolibooruSearch", args="1girl")
-        assert img is not ""
+        assert img
 
 
 def test_nhentai_random():
     with nsfw_dl.NSFWDL() as dl:
         img = dl.download("NhentaiRandom")
-        assert img is not ""
+        assert img
 
 
 def test_rule34_random():
     with nsfw_dl.NSFWDL() as dl:
         img = dl.download("Rule34Random")
-        assert img is not ""
+        assert img
 
 
 def test_rule34_search():
     with nsfw_dl.NSFWDL() as dl:
         img = dl.download("Rule34Search", args="1girl")
-        assert img is not ""
+        assert img
 
 
 def test_tbib_random():
     with nsfw_dl.NSFWDL() as dl:
         img = dl.download("TbibRandom")
-        assert img is not ""
+        assert img
 
 
 def test_tbib_search():
     with nsfw_dl.NSFWDL() as dl:
         img = dl.download("TbibSearch", args="1girl")
-        assert img is not ""
+        assert img
 
 
 def test_tsumino_random():
     with nsfw_dl.NSFWDL() as dl:
         img = dl.download("TsuminoRandom")
-        assert img is not ""
+        assert img
 
 
 def test_xbooru_random():
     with nsfw_dl.NSFWDL() as dl:
         img = dl.download("XbooruRandom")
-        assert img is not ""
+        assert img
 
 
 def test_xbooru_search():
     with nsfw_dl.NSFWDL() as dl:
         img = dl.download("XbooruSearch", args="1girl")
-        assert img is not ""
+        assert img
 
 
 def test_yandere_random():
     with nsfw_dl.NSFWDL() as dl:
         img = dl.download("YandereRandom")
-        assert img is not ""
+        assert img
 
 
 def test_yandere_search():
     with nsfw_dl.NSFWDL() as dl:
         img = dl.download("YandereSearch", args="1girl")
-        assert img is not ""
+        assert img
