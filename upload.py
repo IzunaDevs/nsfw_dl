465b823a5503512503cf93af7853b90b176f2c2f
martmists <martmists@gmail.com>
2017-09-18 23:26:04 +0200

    Add auto-upload to travis

--- upload.py
+++ upload.py
@@ -1,15 +1,22 @@
 import os
 import subprocess
+
+import requests
+
 import nsfw_dl
 
 
 un = os.environ["PYPI_USERNAME"]
 pw = os.environ["PYPI_PASSWORD"]
+
+remote_ver = requests.get(
+    "https://pypi.python.org/pypi/nsfw_dl/json").json()["info"]["version"]
 ver = nsfw_dl.__version__
 
-try:
-    subprocess.Popen("python3.6 setup.py build buld_ext build_whl".split())
-    subprocess.Popen(f"twine upload build/*{ver}* -u {un} -p {pw}".split())
-
-except:
-    pass
+if ver != remote_ver:
+    p1 = subprocess.Popen(
+            "python3.6 setup.py build buld_ext build_whl".split())
+    p1.wait()
+    p2 = subprocess.Popen(
+            f"twine upload build/*{ver}* -u {un} -p {pw}".split())
+    p2.wait()
