78853dfefcf64cc2da4931e48fe6dce803216021
martmists <martmists@gmail.com>
2017-09-19 15:57:27 +0200

    Fix things

--- nsfw_dl/__init__.py
+++ nsfw_dl/__init__.py
@@ -3,11 +3,12 @@ Read the license at:
 https://github.com/IzunaDevs/nsfw_dl/blob/master/LICENSE
 """
 
-from .dl import NSFWDL  # noqa
-from .bases import *  # noqa pylint: disable=wildcard-import
+from .dl import NSFWDL, LOADERS as SOURCES
+from .bases import *
 
 __title__ = 'nsfw_dl'
 __author__ = 'IzunaDevs'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2017 IzunaDevs'
-__version__ = '0.3.1'
+__version__ = '0.3.2'
+__all__ = ["NSFWDL", "SOURCES"]
--- nsfw_dl/__main__.py
+++ nsfw_dl/__main__.py
@@ -3,34 +3,57 @@ Read the license at:
 https://github.com/IzunaDevs/nsfw_dl/blob/master/LICENSE
 """
 import argparse
+import sys
+
 import nsfw_dl
 
 
-def download(downloader, args, file):
+def download(downloader, args, file, download):
     with nsfw_dl.NSFWDL() as dl:
         img = dl.download(downloader, args=args)
 
         if callable(file):
             file = file(img)
 
-        with open(file, "wb") as f:
-            f.write(dl.get(img))
+        if download:
+            with open(file, "wb") as f:
+                f.write(dl.get(img))
+                print(file)
+
+        else:
+            print(img)
 
 
 # TODO: Add more args.
-def main():
+def main(argv):
     """
     Main entrypoint to nsfw_dl commandline.
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--download',
-                        help='Download the result to a file.',
-                        default=False)
-    parser.add_argument('-f', '--file',
-                        help="Filename to download to."
-                        default=lambda x:x.split("/")[-1])
-    ns = parser.parse_args()
+    image = argparse.ArgumentParser()
+    parser.add_argument("action", choices=["image", "sources"])
+
+    image.add_argument('-d', '--download',
+                       help='Download the result to a file.',
+                       default=False, action="store_true")
+    image.add_argument('-f', '--file',
+                       help="Filename to download to.",
+                       default=lambda x: x.split("/")[-1])
+    image.add_argument('source', help="Image source to use.")
+    image.add_argument('query', help="Tags to use during search.",
+                       default='', nargs="*")
+
+    args = parser.parse_args(argv)
+
+    if args.action == "sources":
+        sources = "\n".join("\n".join(v for v in source) for source in
+                            nsfw_dl.SOURCES)
+        print(sources)
+
+    else:
+        args = image.parse_args(argv[1:])
+        download(args.source, args.query, args.file, args.download)
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
