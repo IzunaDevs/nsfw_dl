5add629bdcfb39773ca5e8861312931d762da7e0
AraHaan <seandhunt_7@yahoo.com>
2017-09-18 23:43:51 -0400

    Start on the command line.
    Added an arg parsor.

--- nsfw_dl/__main__.py
+++ nsfw_dl/__main__.py
@@ -2,15 +2,23 @@
 Read the license at:
 https://github.com/IzunaDevs/nsfw_dl/blob/master/LICENSE
 """
-import sys
+import argparse
 
 
+# TODO: Add more args.
 def main(argv):
     """
     Main entrypoint to nsfw_dl commandline.
     """
     type(argv)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-d', '--download',
+        help='Download the result to a file.',
+        default=False)
+    ns = parser.parse_args()
+    type(ns)
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main()
