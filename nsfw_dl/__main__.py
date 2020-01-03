b8c1a3fd0dacd2295a9ce7d3b965bdb771278117
AraHaan <15173749+AraHaan@users.noreply.github.com>
2020-01-03 11:54:04 -0500

    According to Repl.it this works.

--- nsfw_dl/__main__.py
+++ nsfw_dl/__main__.py
@@ -10,50 +10,35 @@ import sys
 import nsfw_dl
 
 
-def download(downloader, args, file, download_file):
-    with nsfw_dl.NSFWDL() as dl:
-        img = dl.download(downloader, args=args)
-
-        if callable(file):
-            file = file(img)
-
-        if download_file:
-            with open(file, "wb") as f:
-                f.write(dl.get(img))
-                print(file)
-
-        else:
-            print(img)
-
-
 def main(argv=sys.argv[1:]):  # pylint: disable=dangerous-default-value
     """
     Main entrypoint to nsfw_dl commandline.
     """
-    parser = argparse.ArgumentParser()
     image = argparse.ArgumentParser()
-    parser.add_argument("action", choices=["image", "sources"])
-
     image.add_argument('-d', '--download',
                        help='Download the result to a file.',
                        default=False, action="store_true")
     image.add_argument('-f', '--file',
                        help="Filename to download to.",
                        default=lambda x: x.split("/")[-1])
-    image.add_argument('source', help="Image source to use.")
-    image.add_argument('query', help="Tags to use during search.",
+    image.add_argument('-s', '--source',
+                       help='Image source to use.',
+                       default='Rule34Random')
+    image.add_argument('query', help='Tags to use during search.',
                        default='', nargs="*")
-
-    args = parser.parse_args(argv)
-
-    if args.action == "sources":
-        sources = "\n".join("\n".join(v for v in source) for source in
-                            nsfw_dl.SOURCES.values())
-        print(sources)
-
-    else:
-        args = image.parse_args(argv[1:])
-        download(args.source, args.query, args.file, args.download)
+    args = image.parse_args(argv[1:])
+    download_file = args.download
+    file = args.file
+    with nsfw_dl.NSFWDL() as dl:
+        img = dl.download(args.source, args=args.query)
+        if callable(file):
+            file = file(img)
+        if download_file:
+            with open(file, "wb") as f:
+                f.write(dl.get(img))
+                print(file)
+        else:
+            print(img)
 
 
 if __name__ == '__main__':
