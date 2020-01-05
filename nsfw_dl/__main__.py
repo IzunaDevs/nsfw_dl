61f78a13362fb75796c423f60a62c865e50e212a
AraHaan <15173749+AraHaan@users.noreply.github.com>
2020-01-05 11:30:23 -0500

    Rewrite command-line. (#9)
    * According to Repl.it this works.
    
    * Fix it to where if they do not provide source it prints out help.
    
    Automatically print out help with no source is provided.
    
    * flake8...
    
    * Fixed bugs in command-line.
    
    Now it should work with ``nsfw_dl -d True -s Rule34Search query`` or with ``nsfw_dl -s Rule32Search query`` perfectly fine.
    
    I however wanted it to be if ``-d`` or if ``--download`` is provided to flip the value of it to true though.
    
    * flake8 again...

--- nsfw_dl/__main__.py
+++ nsfw_dl/__main__.py
@@ -6,54 +6,50 @@ https://github.com/IzunaDevs/nsfw_dl/blob/master/LICENSE
 """
 import argparse
 import sys
+import os
 
 import nsfw_dl
-
-
-def download(downloader, args, file, download_file):
-    with nsfw_dl.NSFWDL() as dl:
-        img = dl.download(downloader, args=args)
-
-        if callable(file):
-            file = file(img)
-
-        if download_file:
-            with open(file, "wb") as f:
-                f.write(dl.get(img))
-                print(file)
-
-        else:
-            print(img)
 
 
 def main(argv=sys.argv[1:]):  # pylint: disable=dangerous-default-value
     """
     Main entrypoint to nsfw_dl commandline.
     """
-    parser = argparse.ArgumentParser()
     image = argparse.ArgumentParser()
-    parser.add_argument("action", choices=["image", "sources"])
-
     image.add_argument('-d', '--download',
                        help='Download the result to a file.',
-                       default=False, action="store_true")
+                       default=False)
     image.add_argument('-f', '--file',
                        help="Filename to download to.",
                        default=lambda x: x.split("/")[-1])
-    image.add_argument('source', help="Image source to use.")
-    image.add_argument('query', help="Tags to use during search.",
+    image.add_argument('-s', '--source',
+                       help='Image source to use.',
+                       default='')
+    image.add_argument('query', help='Tags to use during search.',
                        default='', nargs="*")
-
-    args = parser.parse_args(argv)
-
-    if args.action == "sources":
+    args = image.parse_args(argv)
+    if (args.source == ''):
+        print("Usage: " + os.path.basename(sys.argv[0]) + " [-d/--download]"
+              " [-f/--file ...] [-s/--source ...] [query]")
+        print("Where first ... is the file name you want, second ... "
+              "is the source where source can be:")
         sources = "\n".join("\n".join(v for v in source) for source in
                             nsfw_dl.SOURCES.values())
         print(sources)
-
+        print("And query is what you want to search for.")
     else:
-        args = image.parse_args(argv[1:])
-        download(args.source, args.query, args.file, args.download)
+        download_file = args.download
+        file = args.file
+        with nsfw_dl.NSFWDL() as dl:
+            img = dl.download(args.source, args=" ".join(args.query))
+            if callable(file):
+                file = file(img)
+            if download_file:
+                with open(file, "wb") as f:
+                    f.write(dl.get(img))
+                    print(file)
+            else:
+                print(img)
 
 
 if __name__ == '__main__':
